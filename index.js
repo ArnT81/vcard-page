64a8cf9507f21046cfeab3f7a6f3cb6089da9336
ArnT81 <anders_soderberg@hotmail.com>
2023-06-26 15:40:13 +0200

    swipe function with qrCode on second view

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const addContact = async () => {
 	window.location.href = 'https://arnt.hopto.org/vcard';
-}
+};
+
+const createSwipe = () => {
+	window.mySwipe = new Swipe($('#slider')[0], {
+		// startSlide: 0,
+		speed: 400,
+		// auto: 3000,
+		continuous: true,
+		disableScroll: false,
+		stopPropagation: false,
+		callback: function (index, elem) { },
+		transitionEnd: function (index, elem) { }
+	});
+};
+
 
 $(() => {
-	$('#add_contact').on('mousedown', addContact)
-})
+	createSwipe();
+	$('#add_contact').on('mousedown', addContact);
+});
